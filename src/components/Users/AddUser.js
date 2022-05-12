742a5a898b82928d64c687bbeb2868652826eba4
Duyen Nguyen <hongduyen0705@gmail.com>
2022-05-12 19:07:06 +0300

    cleared input field on form submit by adding value attribute to input element

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
     console.log("add user");
+    console.log(enteredAge, enteredUsername);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+    console.log("name change handler");
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+    console.log("age change handler");
+  };
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          onChange={usernameChangeHandler}
+          id="username"
+          name="username"
+          value={enteredUsername}
+        />
         <label htmlFor="age">Age</label>
-        <input type="number" id="age" name="age" />
+        <input
+          type="number"
+          onChange={ageChangeHandler}
+          id="age"
+          name="age"
+          value={enteredAge}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
